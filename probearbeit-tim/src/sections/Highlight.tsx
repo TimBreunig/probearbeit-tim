7fc363fe4bd5144dec7fc10e675d83fc650e4e2b
TimBreunig <breunig.tim@web.de>
2026-04-02 01:17:33 +0200

    fix: Modified implementation for breakpoints
    - Changed Highlight component breakpoints for multiple styles to ensure clean responsive behavior

--- probearbeit-tim/src/sections/Highlight.tsx
+++ probearbeit-tim/src/sections/Highlight.tsx
@@ -7,14 +7,14 @@ function Highlight() {
   return (
     <div className='container relative py-10'>
       <div className='grid gap-y-0 rounded-lg overflow-hidden'>
-        <div className='col-span-2 md:col-span-8 order-1 md:order-2 md:-ml-8 md:w-[calc(100%+32px)] '>
+        <div className='col-span-2 md:col-span-12 lg:col-span-8 order-1 lg:order-2 lg:-ml-8 lg:w-[calc(100%+32px)] '>
           <img
             src={HighlightImage}
             alt='highlight image'
             className='w-full aspect-video object-cover'
           />
         </div>
-        <div className='col-span-2 md:col-span-4 order-2 md:order-1 flex flex-col gap-6 md:justify-between px-8 py-10 bg-product-f-blue-dark'>
+        <div className='col-span-2 md:col-span-12 lg:col-span-4 order-2 lg:order-1 flex flex-col gap-6 lg:justify-between px-8 py-10 bg-product-f-blue-dark'>
           <div className='flex flex-col gap-4 text-white'>
             <h2 className='h2'>
               Enim nulla facilisis viverra lobortis
