3dbde75cd3f5501eb038bc2f66a18123be672d90
TimBreunig <breunig.tim@web.de>
2026-04-06 02:00:31 +0200

    feat: Added active state and individual styles to buttons
    - Added isActive property to represent persistent active state
    - Added active variant classes that are triggered by isActive
    - Added className for individual styles, e.g. positioning

--- probearbeit-tim/src/components/Button.tsx
+++ probearbeit-tim/src/components/Button.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 
 type BaseProps = {
   children: React.ReactNode
-  onClick?: () => void
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
   iconOnly?: boolean
   disabled?: boolean
+  isActive?: boolean
+  className?: string
 }
 
 type ButtonProps =
@@ -26,33 +28,45 @@ export default function Button({
   onClick,
   iconOnly = false,
   disabled = false,
-}: ButtonProps) {
+  isActive = false,
+  className = '',
+}: ButtonProps & { isActive?: boolean }) {
   const variantClasses = {
     default: `
       bg-gray-70 text-gray-900
       hover:bg-gray-500
       active:bg-gray-70 active:border-gray-900
-      ${disabled && 'bg-gray-70 text-gray-700 cursor-not-allowed'}
+      disabled:bg-gray-70 disabled:text-gray-700 disabled:cursor-not-allowed disabled:border-transparent
     `,
     primary: `
       bg-white text-gray-900
       hover:bg-gray-70
       active:bg-white active:border-gray-900
-      ${disabled && 'bg-white text-gray-700 cursor-not-allowed'}
+      disabled:bg-white disabled:text-gray-700 disabled:cursor-not-allowed disabled:border-transparent
     `,
     secondary: `
       bg-gray-900 text-white
       hover:bg-black
       active:bg-gray-900 active:border-black
-      ${disabled && 'bg-gray-700 text-black cursor-not-allowed'}
+      disabled:bg-gray-700 disabled:text-black disabled:cursor-not-allowed disabled:border-transparent
     `,
     underline: `
       text-gray-900 underline underline-offset-6
       hover:text-black
       active:text-black
-      ${disabled && 'text-gray-700 cursor-not-allowed'}
+      disabled:text-gray-700 disabled:cursor-not-allowed
     `,
   }
+
+
+  // persistent active state for filter buttons
+  const activeVariantClasses = {
+    default: 'bg-gray-70 !border-gray-900',
+    primary: 'bg-white !border-gray-900',
+    secondary: 'bg-gray-900 !border-black',
+    underline: 'text-black',
+  }
+
 
   // can be extended to ensure that lg cannot be applied to variants 'secondary' and 'underline', according to the style guide
   const sizeClasses = {
@@ -65,19 +79,22 @@ export default function Button({
       : 'h-12 px-6',
   }
 
-  const classes = `
-    flex flex-row items-center gap-2 box-border border border-transparent rounded-full button cursor-pointer transition-colors duration-300
-    ${variantClasses[variant]}
-    ${sizeClasses[size]}
-  `
+  const classes = [
+    'flex flex-row items-center gap-2 box-border border border-transparent rounded-full button cursor-pointer transition-colors duration-300',
+    variantClasses[variant],
+    sizeClasses[size],
+    isActive ? activeVariantClasses[variant] : '',
+    className,
+  ].join(' ')
 
   return (
     <button
       type='button'
       className={classes}
       onClick={onClick}
-      disabled={disabled}>
+      disabled={disabled}
+    >
       {children}
     </button>
-  );
+  )
 }
