46d4a18dedce7fbfe75488df170f88eb8aac3e93
TimBreunig <breunig.tim@web.de>
2026-04-06 23:33:32 +0200

    feat: Add mobile navigation
    - Made button clickable
    - Added mobile menu state for interactivity
    - Implemented nav UI for mobile screens

--- probearbeit-tim/src/sections/Header.tsx
+++ probearbeit-tim/src/sections/Header.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+
+import MegaMenu from './MegaMenu'
+import Button from '../components/Button'
+import NavItem from '../components/NavItem'
+import type { LinkProps } from '../components/Link'
 
 import logo from '../assets/images/brand/combination_mark_green.svg'
 import menuIcon from '../assets/icons/ui/menu-8 1.svg'
-
-import MegaMenu from './MegaMenu'
-import NavItem from '../components/NavItem'
-import type { LinkProps } from '../components/Link'
+import crossIcon from '../assets/icons/ui/e-remove 1.svg'
 
 
 const navItems: LinkProps[] = [
@@ -26,6 +29,7 @@ const navItems: LinkProps[] = [
 
 function Header() {
   const [isMegaOpen, setIsMegaOpen] = useState(false)
+  const [isMobileOpen, setIsMobileOpen] = useState(false)
 
   return (
     <div className='w-full container sticky top-0 h-20 md:h-22 flex justify-between items-center bg-white border-b border-gray-70 z-50'>
@@ -34,9 +38,48 @@ function Header() {
       </a>
 
       {/* Mobile Navigation Menu */}
-      <a className='block md:hidden rounded-full hover:bg-gray-70'>
-        <img src={menuIcon} alt='Logoips logo' className='w-12 h-12 p-3.5'/>
-      </a>
+      <Button
+        iconOnly
+        onClick={() => setIsMobileOpen(!isMobileOpen)}
+        className='block md:hidden rounded-full hover:bg-gray-70'
+      >
+        {isMobileOpen
+          ? <img src={crossIcon} alt='Close Menu' className='w-12 h-12 p-3.5'/>
+          : <img src={menuIcon} alt='Open Menu' className='w-12 h-12 p-3.5'/>
+        }
+        
+      </Button>
+
+      <div
+				className={`fixed left-0 right-0 bottom-0 top-20 md:top-22 z-40 bg-white transform transition-transform duration-300 ease-in-out
+          ${isMobileOpen ? "translate-x-0" : "translate-x-full"}
+        `}
+			>
+				<ul className='container flex flex-col items-start'>
+          {navItems.map((item, i) => {
+            const Component = item.component
+
+            if (Component) {
+              return (
+                <li
+                  key={i}
+                  onMouseEnter={() => setIsMegaOpen(true)}
+                  onMouseLeave={() => setIsMegaOpen(false)}
+                >
+                  <NavItem text={item.text} link={item.link} />
+                  {Component && isMegaOpen && <Component />}
+                </li>
+              )
+            }
+
+            return (
+            <li>
+              <NavItem key={i} {...item} />
+            </li>
+          )
+        })}
+      </ul>
+		</div>
 
       {/* Desktop Navigation Menu */}
       <ul className='hidden md:flex items-center'>
